a7045f5e3ba04bf8f3008e26ce79fb3724467611
Akash <therealakash13@gmail.com>
2025-09-29 13:29:16 +0000

    Node and using native filesystem modules

---
+++ 20 Backend/2.2 Native Modules/index.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+// fs.writeFile("message.txt", "Hello from Akash!", function (err) {
+//   if (err) {
+//     console.log(err);
+//   }
+
+//   console.log("File written successfully!");
+// });
+
+// fs.readFile("message.txt", "utf8", function (err, data) {
+//   if (err) {
+//     console.log(err);
+//   }
+
+//   console.log(data);
+// });
