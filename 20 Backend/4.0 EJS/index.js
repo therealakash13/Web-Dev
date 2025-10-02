4cbe17c976b726df8785ef5177aba48533a7791f
Akash <therealakash13@gmail.com>
2025-10-02 08:52:39 +0000

    EJS

--- 20 Backend/4.0 EJS/index.js
+++ 20 Backend/4.0 EJS/index.js
@@ -5,12 +5,15 @@ const port = 3000;
 const host = "localhost";
 
 const app = express();
+app.set("view engine", "ejs");
+
 const day = new Date();
 const today = day.getDay();
-let t = "a weekday";
-let adv = "work hard.";
 
 app.get("/", function (req, res) {
+  let t = "a weekday";
+  let adv = "work hard.";
+
   if (today === 0 || today === 6) {
     t = "the weekend";
     adv = "have fun";
