d9c0b90421cada88e1c249a66d2db63556b20e40
Akash <therealakash13@gmail.com>
2025-10-05 09:10:21 +0000

    Secrets Project

---
+++ 20 Backend/5.6 Secrets Project/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  const response = await axios.get("https://secrets-api.appbrewery.com/random");
+  console.log(response.data);
+
+  res.render("index.ejs", {
+    secret: response.data.secret,
+    user: response.data.username,
+  });
+});
+
+app.listen(port, () => console.log(`App is running at ${port}.`));
